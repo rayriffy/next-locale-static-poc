5f12c3957fb60932c9295e61267fc45709cb4379
Phumrapee Limpianchop <git@rayriffy.com>
2021-05-17 06:51:06 +0900

    chore: staged changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,31 +16,11 @@ const Page: NextPage = props => {
   return (
     <div className="container mx-auto pt-12">
       <h1 className="font-semibold text-4xl pb-2">Riffy's Next locale demo</h1>
-      <p>
-        inb4 Next with dynamic routes when we{' '}
-        <code className="bg-black text-white text-sm p-1 rounded">
-          next export
-        </code>{' '}
-        it should be able to navigate dynamically by configure <b>redirects</b>{' '}
-        correctly. (
-        <a
-          className="text-blue-500"
-          href="https://github.com/Brikl/opensource/tree/master/libs/netlify-plugin-next-dynamic"
-        >
-          More info about this
-        </a>
-        )
-      </p>
-      <p>
-        With experimental i18n in Next 9.5, it cannot do like that due to how{' '}
-        <code className="bg-black text-white text-sm p-1 rounded">
-          useRouter()
-        </code>{' '}
-        works (and it experimental). But let's see with the power with Next 10!
-      </p>
       <div className="border-b py-2"></div>
       <h2 className="font-medium text-2xl py-4">{locales[router.locale]}</h2>
-      <p>Detected locales: {router.locale}</p>
+      <p>
+        Detected locales: <b>{router.locale}</b>
+      </p>
       <p>Available locales: {router.locales?.join(', ')}</p>
       <div className="border-b py-2"></div>
       <div className="font-bold py-4">Buttons! Many buttons!!!</div>
